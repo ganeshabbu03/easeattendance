247e04ad4ff47c816d208bbed68091e8d8fb8ec8
ganeshabbu <108935028+ganeshabbu@users.noreply.github.com>
2025-11-30 17:54:38 +0530

    Configure for Vercel deployment

---
+++ api/index.ts
@@ -0,0 +1,19 @@
+import { app, setupApp } from "../server/app";
+import mongoose from "mongoose";
+
+let isReady = false;
+
+export default async (req: any, res: any) => {
+    if (!isReady) {
+        if (mongoose.connection.readyState === 0) {
+            const mongoUri = process.env.MONGODB_URI;
+            if (!mongoUri) {
+                throw new Error("MONGODB_URI environment variable is not defined");
+            }
+            await mongoose.connect(mongoUri);
+        }
+        await setupApp();
+        isReady = true;
+    }
+    app(req, res);
+};
